ee26293c24495baef394fabd7fd44298a75cdc32
gyhuminyan <860893330@qq.com>
2019-01-16 17:33:11 +0800

    word count

---
+++ spark-ml/src/main/scala/ChiSquareTestExample.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.ml.linalg.{ Vectors,Vector}
+import org.apache.spark.ml.stat.ChiSquareTest
+import org.apache.spark.sql.SparkSession
+
+object ChiSquareTestExample {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .master("local[*]")
+      .appName("CorrelationExample")
+      .getOrCreate()
+    import spark.implicits._
+
+    val data = Seq(
+      (0.0, Vectors.dense(0.5, 10.0)),
+      (0.0, Vectors.dense(1.5, 20.0)),
+      (1.0, Vectors.dense(1.5, 30.0)),
+      (0.0, Vectors.dense(3.5, 30.0)),
+      (0.0, Vectors.dense(3.5, 40.0)),
+      (1.0, Vectors.dense(3.5, 40.0))
+    )
+
+    val df = data.toDF("label", "features")
+    val chi = ChiSquareTest.test(df, "features", "label").head
+    println(s"pValues = ${chi.getAs[Vector](0)}")
+    println(s"degreesOfFreedom ${chi.getSeq[Int](1).mkString("[", ",", "]")}")
+    println(s"statistics ${chi.getAs[Vector](2)}")
+  }
+}
